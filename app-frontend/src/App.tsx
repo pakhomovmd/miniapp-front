45b9acbf82368c1cca850b95e60c7808f8f0e12e
Pakhomov Michael <pakhomovmichael@icloud.com>
2025-11-21 02:32:41 +0400

    Update App.tsx

--- app-frontend/src/App.tsx
+++ app-frontend/src/App.tsx
@@ -20,6 +20,8 @@ export default function App() {
   const API_URL =
     import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
+  console.log("API_URL:", API_URL);
+  
   async function refresh() {
   setShowLoader(true); // показываем лоадер
   setData(null);
